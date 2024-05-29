8eee6f71d23c1b583eb634985290b140ef0224d9
alphy-17 <102354173+alphy-17@users.noreply.github.com>
2024-05-29 10:51:38 +1000

    Add files via upload

---
+++ trendallfe/tailwind.config.js
@@ -0,0 +1,76 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+  flowbite.content(),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: "#FBFBFB",
+          200: "#EAEAEA",
+          300: "#DFDFDF",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#333333",
+          900: "#191919",
+        },
+        pallet1:{
+          50:"#E9DFD0",
+          51:"#BBBDBC"
+        },
+        //primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      },
+      fontFamily: {
+        'body': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ],
+        'sans': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin(),
+    require('@tailwindcss/forms'),
+    
+  ],
+}
+
