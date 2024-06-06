cf1304fc882c9bc000afc34bc2c64b871be053f3
alphy-17 <102354173+alphy-17@users.noreply.github.com>
2024-06-06 17:52:30 +1000

    Update reportWebVitals.js

--- trendallfe/src/reportWebVitals.js
+++ trendallfe/src/reportWebVitals.js
@@ -1,13 +1,35 @@
+// Author : Ifteker Azim
+// Last revised : 01-06-2024
+
+/**
+ * Reports web vitals performance metrics.
+ *
+ * This function loads the 'web-vitals' library and reports various web performance metrics 
+ * (Cumulative Layout Shift, First Input Delay, First Contentful Paint, Largest Contentful Paint, 
+ * and Time to First Byte) using the provided callback function `onPerfEntry`.
+ *
+ * @param {Function} onPerfEntry - A callback function that will receive the performance metrics.
+ * It is executed if and only if it is a function.
+ */
+
 const reportWebVitals = onPerfEntry => {
+  // Check if onPerfEntry is provided and is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the 'web-vitals' library
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Measure and report Cumulative Layout Shift (CLS)
       getCLS(onPerfEntry);
+      // Measure and report First Input Delay (FID)
       getFID(onPerfEntry);
+      // Measure and report First Contentful Paint (FCP)
       getFCP(onPerfEntry);
+      // Measure and report Largest Contentful Paint (LCP)
       getLCP(onPerfEntry);
+      // Measure and report Time to First Byte (TTFB)
       getTTFB(onPerfEntry);
     });
   }
 };
 
+// Export the reportWebVitals function as the default export of this module
 export default reportWebVitals;
