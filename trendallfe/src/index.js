50a3e2e2376d33786d971b45a01b7b2e6c6c532f
alphy-17 <102354173+alphy-17@users.noreply.github.com>
2024-06-06 17:51:54 +1000

    Update index.js

--- trendallfe/src/index.js
+++ trendallfe/src/index.js
@@ -1,11 +1,25 @@
+// Author : Ifteker Azim
+// Last revised : 01-06-2024
+
+// Importing React library to use JSX and create React components
 import React from 'react';
+// Importing ReactDOM to interact with the DOM in the browser
 import ReactDOM from 'react-dom/client';
+// Importing global CSS styles for the application
 import './index.css';
+// Importing the main App component
 import App from './App';
+// Importing a utility to report web vitals (performance metrics)
 import reportWebVitals from './reportWebVitals';
+// Importing ThemeProvider from Material Tailwind to provide theme context to the app
 import { ThemeProvider } from "@material-tailwind/react";
 
+// Creating a root element to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the React application inside the root element
+// React.StrictMode is a wrapper to highlight potential problems in an application
+// ThemeProvider provides the theme context to the App component and its children
 root.render(
   <React.StrictMode>
     <ThemeProvider>
@@ -14,7 +28,11 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/**
+ * Function to report web vitals (performance metrics) of the app.
+ * It helps in measuring the performance of the app.
+ * You can pass a function to log results or send them to an analytics endpoint.
+ * Learn more: https://bit.ly/CRA-vitals
+ */
 reportWebVitals();
+
